03fd37ed1fe110e7837d03fa9b9079a005b675d8
Ademilton Barbosa <ademiltonbarbosadev@gmail.com>
2024-02-13 11:20:23 -0300

    1 commit

---
+++ meu-script/main.js
@@ -0,0 +1,40 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "imagens/sayajins.png") {
+    myImage.setAttribute("src", "imagens/dbz.jpg");
+  } else {
+    myImage.setAttribute("src", "imagens/sayajins.png");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Por favor, digite o seu nome");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Dragon Ball é legal, ${myName}`;
+  }
+
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Dragon Ball é legal, ${storedName}`;
+  }
+
+  myButton.onclick = () => {
+    setUserName();
+  };
+
+  function setUserName() {
+    const myName = prompt("Por favor digite o seu nome.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Dragon Ball é legal, ${myName}`;
+    }
+  }
